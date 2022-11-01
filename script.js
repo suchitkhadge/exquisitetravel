08d9f819250478370bcd180f4bcc313fd6319f27
yaping0816 <yaping0816@gmail.com>
2022-11-01 16:04:40 -0700

    attractions button is working

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ const rate = 3;
 const limit = 10;
 // https://api.opentripmap.com/0.1/en/places/autosuggest?name=att&radius=5000&lon=-122.33207&lat=47.60621&rate=3&limit=10&apikey=5ae2e3f221c38a28845f05b697184d3cd9ee672b578170059a3aa7e6
 
-// "https://api.opentripmap.com/0.1/en/places/autosuggest?name=att&radius=5000&lon=undefined&lat=undefined&rate=3&limit=10&apikey=5ae2e3f221c38a28845f05b697184d3cd9ee672b578170059a3aa7e6"
-
-const attractionsAPi = `${openTripBaseURL}autosuggest?name=att&radius=${radius}&lon=${geo[1]}&lat=${geo[0]}&rate=${rate}&limit=${limit}&apikey=${opentripApiKey}`;
-
 async function callOpenTripApiToGetGeo(api) {
   const response = await axios.get(api);
   const lat = response.data.lat;
@@ -24,31 +20,37 @@ myForm.addEventListener("submit", async (e) => {
   const openTripAPI = `${openTripBaseURL}geoname?&name=${city}&apikey=${opentripApiKey}`;
   geo = await callOpenTripApiToGetGeo(openTripAPI);
   console.log(geo);
+
+  attractionBtn.addEventListener("click", showAttractionsList);
 });
 
-// const geo =
+function showAttractionsList() {
+  console.log(geo);
+  const attractionsAPi = `${openTripBaseURL}autosuggest?name=att&radius=${radius}&lon=${geo[1]}&lat=${geo[0]}&rate=${rate}&limit=${limit}&apikey=${opentripApiKey}`;
+  callOpenTripApiToGetAttractionsList(attractionsAPi);
+}
 
 async function callOpenTripApiToGetAttractionsList(api) {
-  //   const list = document.createElement("ul");
-  //   const main_container = document.querySelector("#main-container");
-  //   main_container.appendChild(list);
-  debugger;
+  const list = document.createElement("ul");
+  const main_container = document.querySelector("#main-container");
+  main_container.appendChild(list);
+  //   debugger;
   console.log(geo[0]);
   const response = await axios.get(api);
   const data = response.data.features;
   console.log(data);
-  //   for (const attraction of data) {
-  //     debugger;
-  //     const listEle = document.createElement("li");
-  //     listEle.textContent = attraction.properties.name;
-  //     list.appendChild(listEle);
-  //     const btn = document.createElement("button");
-  //     btn.textContent = "Details";
-  //     listEle.appendChild(btn);
-  //   }
+  for (const attraction of data) {
+    // debugger;
+    const listEle = document.createElement("li");
+    listEle.textContent = attraction.properties.name;
+    list.appendChild(listEle);
+    const btn = document.createElement("button");
+    btn.textContent = "Details";
+    listEle.appendChild(btn);
+  }
 }
 
-attractionBtn.addEventListener("click", async () => {
-  debugger;
-  await callOpenTripApiToGetAttractionsList(attractionsAPi);
-});
+// attractionBtn.addEventListener("click", async () => {
+//   debugger;
+//   await await callOpenTripApiToGetAttractionsList(attractionsAPi);
+// });
