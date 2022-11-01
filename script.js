a6c43fc07ebe247ab79a9be85c6f6469239a5cee
yaping0816 <yaping0816@gmail.com>
2022-11-01 12:54:40 -0700

    added a geo varaible to store the value

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const BaseURL = "https://api.opentripmap.com/0.1/en/places/geoname";
 const opentripApiKey =
   "5ae2e3f221c38a28845f05b697184d3cd9ee672b578170059a3aa7e6";
 
+let geo = [];
+
 myForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const city = user_input.value;
   const openTripAPI = `${BaseURL}?&name=${city}&apikey=${opentripApiKey}`;
-  console.log(await callOpenTripApi(openTripAPI));
+  geo = await callOpenTripApi(openTripAPI);
+  console.log(geo);
 });
 
 async function callOpenTripApi(api) {
