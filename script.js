eb3282ecb3de1b5db7c0757978fd65f164d51fac
yaping0816 <yaping0816@gmail.com>
2022-11-02 13:28:24 -0700

    hard coding the picture description and address of attraction card

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ myForm.addEventListener("submit", async (e) => {
 console.log(city);
 
 function showAttractionsList() {
-  //   console.log(city);//???why it's not none
   const att = city.slice(0, 3);
   // const attractionsAPi = `${openTripBaseURL}autosuggest?name=${att}&radius=${radius}&lon=${geo[1]}&lat=${geo[0]}&rate=${rate}&limit=${limit}&apikey=${opentripApiKey}`;
   const attractionsAPi = `${openTripBaseURL}radius?radius=${radius}&lon=${geo[1]}&lat=${geo[0]}&limit=${limit}&apikey=${opentripApiKey}`;
@@ -51,31 +50,30 @@ async function callOpenTripApiToGetAttractionsList(api) {
     const attractionName = attraction.properties.name;
     listEle.textContent = attractionName;
     list.appendChild(listEle);
-    // const btn = document.createElement("button");
-    // btn.textContent = "Details";
-    const btn = detailsBtnCreator();
+    const id = attractionName.slice(0, 2);
+    const btn = detailsBtnCreator(id);
     listEle.appendChild(btn);
-    const modal = modalCreator(attractionName);
+    const modal = modalCreator(id, attractionName);
     listEle.appendChild(modal);
   }
 }
 
-function detailsBtnCreator() {
+function detailsBtnCreator(id) {
   const btn = document.createElement("button");
   btn.textContent = "Details";
   btn.setAttribute("type", "button");
   btn.classList.add("btn");
   btn.classList.add("btn-primary");
   btn.setAttribute("data-bs-toggle", "modal");
-  btn.setAttribute("data-bs-target", "#exampleModal");
+  btn.setAttribute("data-bs-target", `#${id}`);
   return btn;
 }
 
-function modalCreator(title) {
+function modalCreator(id, title) {
   const firstDiv = document.createElement("div");
   firstDiv.classList.add("modal");
   firstDiv.classList.add("fade");
-  firstDiv.setAttribute("id", "exampleModal");
+  firstDiv.setAttribute("id", id);
   firstDiv.setAttribute("tabindex", "-1");
   firstDiv.setAttribute("aria-labelledby", "exampleModalLabel");
   firstDiv.setAttribute("aria-hidden", "true");
@@ -92,14 +90,79 @@ function modalCreator(title) {
   fourthDiv.classList.add("modal-header");
   thirdDiv.appendChild(fourthDiv);
 
-  const h1Heading = document.createElement("h1");
-  h1Heading.classList.add("modal-title");
-  h1Heading.classList.add("fs-5");
-  h1Heading.setAttribute("id", "exampleModalLabel");
-  h1Heading.textContent = title;
-  fourthDiv.appendChild(h1Heading);
+  const h5Heading = document.createElement("h5");
+  h5Heading.classList.add("modal-title");
+  h5Heading.classList.add("fs-5");
+  h5Heading.setAttribute("id", "exampleModalLabel");
+  console.log("title is", title);
+  // debugger;
+  h5Heading.textContent = title;
+  fourthDiv.appendChild(h5Heading);
+
+  const closeBtn = document.createElement("button");
+  closeBtn.setAttribute("type", "button");
+  closeBtn.classList.add("btn-close");
+  closeBtn.setAttribute("data-bs-dismiss", "modal");
+  closeBtn.setAttribute("aria-label", "Close");
+  fourthDiv.appendChild(closeBtn);
+
+  // const bodyDiv = document.createElement("div");
+  const url =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Seattle_Central_Library%2C_Seattle%2C_Washington_-_20060418.jpg/300px-Seattle_Central_Library%2C_Seattle%2C_Washington_-_20060418.jpg";
+  const des =
+    "The Seattle Central Library is the flagship library of the Seattle Public Library system. The 11-story (185 feet or 56.9 meters high) glass and steel building in downtown Seattle, Washington was opened to the public on May 23, 2004.";
+  const add = "3342,seattle";
+
+  const cardDiv = CardDivCreator(url, des, add);
+  thirdDiv.appendChild(cardDiv);
+
+  const footerDiv = document.createElement("div");
+  footerDiv.classList.add("modal-footer");
+  thirdDiv.appendChild(footerDiv);
+
+  const footerBtn = document.createElement("button");
+  footerBtn.setAttribute("type", "button");
+  addClassName(footerBtn, "btn");
+  addClassName(footerBtn, "btn-secondary");
+  footerBtn.setAttribute("data-bs-dismiss", "modal");
+  footerBtn.textContent = "Close";
+  footerDiv.appendChild(footerBtn);
 
   return firstDiv;
+}
+function addClassName(ele, className) {
+  return ele.classList.add(className);
+}
+
+function CardDivCreator(imgUrl, description, address) {
+  const cardDiv = document.createElement("div");
+  addClassName(cardDiv, "card");
+  // cardDiv.setAttribute("style", "width: 18rem;");
+
+  const image = document.createElement("img");
+  addClassName(image, "card-img-top");
+  image.setAttribute("src", imgUrl);
+  cardDiv.appendChild(image);
+
+  // const cardBodyDiv = document.createElement("div");
+  // addClassName(cardBodyDiv, "card-body");
+  // cardDiv.appendChild(cardBodyDiv);
+  const list = document.createElement("ul");
+  addClassName(list, "list-group");
+  addClassName(list, "list-group-flush");
+  cardDiv.appendChild(list);
+
+  const listItem1 = document.createElement("li");
+  addClassName(listItem1, "list-group-item");
+  listItem1.textContent = description;
+  list.appendChild(listItem1);
+
+  const listItem2 = document.createElement("li");
+  addClassName(listItem2, "list-group-item");
+  listItem2.textContent = address;
+  list.appendChild(listItem2);
+
+  return cardDiv;
 }
 
 function getGoogleMap(city) {
